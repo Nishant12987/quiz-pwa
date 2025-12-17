ee9233ff4d9a0cb14819712c9eb6decdb9bb3dd8
Nishant12987 <151148726+Nishant12987@users.noreply.github.com>
2025-12-17 10:17:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,11 @@
-self.addEventListener("install", e => {
+self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open("reet-pwa").then(c =>
-      c.addAll(["./","./index.html","./css/style.css","./js/app.js"])
-    )
+    caches.open("prepone").then(c=>c.addAll([
+      "./","./index.html","./css/style.css","./js/app.js"
+    ]))
   );
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
