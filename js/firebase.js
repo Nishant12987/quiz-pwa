a5af65047278d3d92ba85e2f62e1c36c11aa0879
Nishant12987 <151148726+Nishant12987@users.noreply.github.com>
2025-12-19 15:49:14 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyD04D7s8Y4ZCUxWE08cYetErY0SXA2hcb0",
   authDomain: "prepone-92297.firebaseapp.com",
   projectId: "prepone-92297",
   storageBucket: "prepone-92297.firebasestorage.app",
   messagingSenderId: "208806915220",
-  appId: "1:208806915220:web:a54fbca8db1dc88c2e1881",
-  measurementId: "G-TRTM2VX9E5"
-};
+  appId: "1:208806915220:web:a54fbca8db1dc88c2e1881"
+});
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
+const auth = firebase.auth();
+const db = firebase.firestore();
